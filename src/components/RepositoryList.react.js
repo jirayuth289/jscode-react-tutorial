d4676d552c46c8d3f3aa32bec986de460ba4cda7
jirayuth26169 <jirayuth.si.56@ubu.ac.th>
2017-08-14 11:07:52 +0700

    Initial Structure Components

---
+++ src/components/RepositoryList.react.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RepositoryListItem from './RepositoryListItem.react'
+
+class RepositoryList extends React.Component {
+    render() {
+        return (
+            <RepositoryListItem />
+        )
+    }
+}
+
+export default RepositoryList
---
+++ src/components/RepositoryListItem.react.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class RepositoryListItem extends React.Component {
+    render() {
+        return (
+            <div>List Item</div>
+        )
+    }
+}
+
+export default RepositoryListItem
